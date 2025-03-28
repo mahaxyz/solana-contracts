2b70e0bc4ebb13c6553425c8adeaeada8d930e54
obchain <riteshnikhoriya94@gmail.com>
2025-03-28 18:34:49 +0530

    feat: added lib.rs

---
+++ programs/solana-tax-token-anchor/src/lib.rs
@@ -0,0 +1,35 @@
+use anchor_lang::prelude::*;
+mod instructions;
+use instructions::*;
+
+declare_id!("2uTRyNTYcwZSWkm9hqxS6gXKdNz67avDRKZzqDesJdeb");
+
+#[program]
+pub mod solana_tax_token_anchor {
+    use super::*;
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        transfer_fee_basis_point: u16,
+        maximum_fee: u64,
+    ) -> Result<()> {
+        process_initialize(ctx, transfer_fee_basis_point, maximum_fee)
+    }
+    pub fn withdraw(ctx: Context<Withdraw>) -> Result<()> {
+        process_withdraw(ctx)
+    }
+
+    pub fn transfer(ctx: Context<Transfer>, amount: u64) -> Result<()> {
+        process_transfer(ctx, amount)
+    }
+    pub fn harvest<'info>(ctx: Context<'_, '_, 'info, 'info, Harvest<'info>>) -> Result<()> {
+        process_harvest(ctx)
+    }
+
+    pub fn update_fee(
+        ctx: Context<UpdateFee>,
+        transfer_fee_basis_points: u16,
+        maximum_fee: u64,
+    ) -> Result<()> {
+        process_update_fee(ctx, transfer_fee_basis_points, maximum_fee)
+    }
+}
